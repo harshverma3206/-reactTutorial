c6e6e9542f58d263f3330c8940b4243f8fe5527d
Harsh <harshverma3206@gmail.com>
2025-11-22 11:40:36 +0530

    first commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <main>
+      <h1>Jataria</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus, doloribus.</p>
+    </main>
+    </>
+  )
+}
+
+export default App
