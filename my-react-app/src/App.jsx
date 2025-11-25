dbbb84721c89e6a587588486c16b6967c1dfe21a
Harsh <harshverma3206@gmail.com>
2025-11-25 15:14:40 +0530

    Second Commit

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
     <main>
       <h1>Jataria</h1>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus, doloribus.</p>
+      <a href="">Completed</a>
     </main>
     </>
   )
